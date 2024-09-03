7161de65001679fee4059105f6c71fccb191be29
sanjayraoms <sanjayraoms@gmail.com>
2024-09-03 17:11:04 +0530

    End of section 6

---
+++ client/src/app/members/member-detail/member-detail.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-menber-detail',
+  standalone: true,
+  imports: [],
+  templateUrl: './member-detail.component.html',
+  styleUrl: './member-detail.component.css'
+})
+export class MemberDetailComponent {
+
+}
